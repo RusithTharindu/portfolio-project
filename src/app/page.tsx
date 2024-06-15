dd4d4b3a805b4c36d4bb9588099d93b6adec6412
RusithTharindu <rusiththarindu2@gmail.com>
2024-06-15 23:47:49 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-white text-[25px] font-semibold">
-      This is Rusith's Portfolio
+      This is Rusith&apos;s Portfolio
     </div>
   )
 }
