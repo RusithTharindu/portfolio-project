0e37b357876fc41b7be22e8d732a5bacfaa6839d
RusithTharindu <rusiththarindu2@gmail.com>
2024-08-16 15:15:42 +0530

    feat: Test loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Container from "@/layout/container";
+"use client"
+
 import Hero from "@/components/hero";
+import Loader from "@/components/loader";
 
 export default function Home() {
   return (
     <div>
+      <Loader />
       <Hero />
     </div>
-  )
+  );
 }
---
+++ src/components/loader.tsx
@@ -0,0 +1,25 @@
+"use state";
+
+import React from "react";
+import { useEffect } from "react";
+
+const loader = () => {
+  useEffect(() => {
+    const loader = document.querySelector(".loader");
+    if (loader) {
+      loader.classList.add("loader--hidden");
+
+      loader.addEventListener("transitionend", () => {
+        document.body.removeChild(loader);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="loader w-full h-screen overscroll-none flex flex-col text-[25px] font-semibold text-white disabled">
+      <p className="mb-5">Loading</p>
+    </div>
+  );
+};
+
+export default loader;
